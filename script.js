22946e9f5bd7ca096d02703349ffa58d1a3aad89
Sarthak2905 <sarthakhendre61@gmail.com>
2025-06-16 17:04:08 +0530

    Set up initial JS variables and DOM element references

---
+++ script.js
@@ -0,0 +1,10 @@
+//Stores references to your HTML elements.
+const input = document.getElementById("task-input");
+const addBtn = document.getElementById("add-task");
+const list = document.getElementById("task-list");
+const taskCount = document.getElementById("task-count");
+const progress = document.querySelector(".progress");
+
+
+let tasks = [];
+
